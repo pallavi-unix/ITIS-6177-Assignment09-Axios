435c2c3d19399ef5ee7762475306dce0a1253582
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2023-10-08 01:16:24 -0400

    Committing code

---
+++ my-favorite-band.js
@@ -0,0 +1,10 @@
+// The below is deployed over aws service
+
+export const handler = async (favorite) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Pallavi Deshmukh favorite band is '+ favorite.keyword),
+    };
+    return response;
+};
---
+++ my-function.js
@@ -0,0 +1,10 @@
+// The below is deployed over aws service
+
+export const handler = async (say) => {
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('Pallavi Deshmukh says '+ say.keyword),
+    };
+    return response;
+};
